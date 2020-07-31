1c3fc2e35d0a019f19639e09f7b1b47f16188ebf
mndlovu18 <mndlovu@brynmawr.edu>
2020-07-31 10:55:35 -0400

    Final

---
+++ form.js
@@ -0,0 +1,34 @@
+var myform = $("form#myform");
+myform.submit(function(event){
+	event.preventDefault();
+
+	var params = myform.serializeArray().reduce(function(obj, item) {
+     obj[item.name] = item.value;
+     return obj;
+  }, {});
+
+  // Change to your service ID, or keep using the default service
+  var service_id = "default_service";
+
+  var template_id = "confirmation";
+  myform.find("button").text("Sending...");
+  emailjs.send(service_id, template_id, params)
+  	.then(function(){ 
+       alert("Sent!");
+       myform.find("button").text("Send");
+     }, function(err) {
+       alert("Send email failed!\r\n Response:\n " + JSON.stringify(err));
+       myform.find("button").text("Send");
+    });
+
+  return false;
+});
+
+
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
--- mainscript.js
+++ mainscript.js
@@ -1,6 +1,6 @@
 window.addEventListener('load', function(){
 	
-	setTimeout(myFunction, 3000)
+	setTimeout(myFunction, 2000)
  })
  
 function myFunction() {
@@ -19,34 +19,41 @@ document.getElementById("myBtn_7").addEventListener('click', mySeventhFunction);
 
 function myFirstFunction(){
 	document.getElementById("fact_1").style.visibility="visible";
+	document.getElementById("myBtn_1").style.background="#ffcad4";
 
 }
 function mySecondFunction(){
 	document.getElementById("fact_2").style.visibility="visible";
+	document.getElementById("myBtn_2").style.background="#ffcad4";
 
 }
 function myThirdFunction(){
 	document.getElementById("fact_3").style.visibility="visible";
+	document.getElementById("myBtn_3").style.background="#ffcad4";
 
 }
 
 function myForthFunction(){
 	document.getElementById("fact_4").style.visibility="visible";
+	document.getElementById("myBtn_4").style.background="#ffcad4";
 
 }
 
 function myFifthFunction(){
 	document.getElementById("fact_5").style.visibility="visible";
+	document.getElementById("myBtn_5").style.background="#ffcad4";
 
 }
 
 function mySixthFunction(){
 	document.getElementById("fact_6").style.visibility="visible";
+	document.getElementById("myBtn_6").style.background="#ffcad4";
 
 }
 
 function mySeventhFunction(){
 	document.getElementById("fact_7").style.visibility="visible";
+	document.getElementById("myBtn_7").style.background="#ffcad4";
 
 }
 
